7459ac302d42b95d49487fd031dbc4b232ebc1f2
basecode360 <basecode41@gmail.com>
2025-06-28 14:00:58 -0400

    Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://api.17autoparts.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path, // Keep the /api prefix
